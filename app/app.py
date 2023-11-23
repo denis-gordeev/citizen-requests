4bc929891e55b21ed2418924f25fd1b630202d96
Alex <k142@mail.ru>
2023-11-24 01:29:00 +0300

    get claim - beta

--- app/app.py
+++ app/app.py
@@ -1,11 +1,19 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, abort, jsonify
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/claim/api/v1.0/text', methods=['POST'])
+def perform_claim():
+    if not request.json or not 'body' in request.json:
+        abort(400)
+    text = request.json['body']
+    return jsonify({'body': text}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
